8d217329d2aa5b8fbaf9d010f1f07f96690ef430
tivmof <tomyivanov06@gmail.com>
2025-03-12 16:58:32 +0000

    feat: Generating form

---
+++ codbex-lead-customer-ext/gen/customer-opportunity-generate/forms/customer-opportunity-generate/controller.js
@@ -0,0 +1,15 @@
+const formView = angular.module('forms', ['ideUI', 'ideView']);
+
+formView.controller('FormController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.forms = {
+        form: {}
+    };
+
+    $scope.model = {};
+    $scope.model. = 0;
+    $scope.model. = '';
+    $scope.model. = '';
+    $scope.model. = '';
+    $scope.model. = '';
+}]);
